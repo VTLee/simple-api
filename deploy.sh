635207b5dd27eccbbae955298088b6d6f295d33c
Lee Doughty <ldoughty@vt.edu>
2019-05-07 08:41:52 -0400

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-export TEMP_CREDENTIALS=$(aws sts assume-role --role-arn arn:aws:iam::a:role/cicd --role-session-name $CI_PIPELINE_ID)
+export TEMP_CREDENTIALS=$(aws sts assume-role --role-arn arn:aws:iam:::role/cicd --role-session-name simple-api-deploy-session)
 export AWS_ACCESS_KEY_ID=$(echo ${TEMP_CREDENTIALS} | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo ${TEMP_CREDENTIALS} | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo ${TEMP_CREDENTIALS} | jq -r .Credentials.SessionToken)
@@ -16,4 +16,4 @@ aws s3 cp dist.zip s3://$S3_BUCKET/$API_NAME/$API_NAME.zip --quiet
 
 sam deploy --template-file sam-ddb-template.yml --stack-name table-stack --no-fail-on-empty-changeset
 
-sam deploy --template-file sam-api-template.yml --stack-name API-$API_NAME --capabilities CAPABILITY_IAM  --parameter-overrides $(cat authentication.poc.env) --no-fail-on-empty-changeset
+sam deploy --template-file sam-api-template.yml --stack-name API-$API_NAME --capabilities CAPABILITY_IAM  --parameter-overrides $(cat secrets.dev.env) --no-fail-on-empty-changeset
